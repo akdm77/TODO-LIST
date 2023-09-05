303221723018c8047c5fdc16339088496aff376d
Alan Klysman Dias Macedo <alanklysmandiasmacedo@pop-os.localdomain>
2023-09-05 18:16:01 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from 'react';
+import './App.css';
+import Filter from './components/Filter';
+import Search from './components/Search';
+import Todo from "./components/Todo";
+import TodoForm from './components/TodoForm';
+
+function App() {
+  const [search, setSearch] = useState("")
+  const [filter, setFilter] = useState("All")
+  const [ sort, setSort] = useState("Asc")
+  const [todos, setTodos] = useState(
+[    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudos React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text, category) => {
+    const newTodos = [...todos,{
+      id: Math.floor(Math.random() * 10000),
+      text,
+      category,
+      isCompleted: false,
+    },
+  ]
+  setTodos(newTodos)
+  }
+
+  const removeTodo = (id) => {
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter((todo) =>
+    todo.id !== id ? todo : null)
+    setTodos(filteredTodos)
+  }
+  
+  const completeTodo = (id) => {
+    const newTodos = [...todos]
+    newTodos.map((todo) => 
+    todo.id === id ? todo.isCompleted = !todo.isCompleted : todo)
+    setTodos(newTodos)
+  }
+  return (
+    <div className='app'>
+      <h1>Lista de tarefas</h1>
+      <Search search={search} setSearch={setSearch}/>
+      <Filter filter = {filter} setFilter = {setFilter} setSort={setSort}/>
+      <div className="todo-list">
+        {todos
+        .filter((todo) => todo.text.toLowerCase().includes(search.toLowerCase()))
+
+        .filter((todo) => filter === "All" 
+        ? true : filter === "Completed" 
+        ? todo.isCompleted 
+        : !todo.isCompleted)
+        
+        .sort((a, b) => sort === "Asc" 
+        ? a.text.localeCompare(b.text)
+        : b.text.localeCompare(a.text))
+
+        .map((todo) => (
+          <Todo key={todo.id} 
+          todo = {todo}
+          completeTodo = {completeTodo} 
+          removeTodo = {removeTodo}/>
+          ))}
+        </div>
+      <TodoForm addTodo = {addTodo} />
+    </div>
+)
+}
+
+export default App
---
+++ src/node_5a0a6d391ed1a.tmp.js
@@ -0,0 +1,128 @@
+import { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState(
+    {
+      id: 1,
+      text: "Criar funcionalidade x no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudos React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  )
+  return (
+    <div className='app'>
+      <h1>Lista de tarfas</h1>
+      { key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMapdos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+todos.map((todo) =>(
+        <div key={todo.id} className="todo"> key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+
+          <div className="content"> key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map() key={id}tofoTransformStrea{todo)import './App.css';
+import './App.css';
+.maMap<todos className="Map"></t, setTodos.Mapmtotodos.m.map()
+            <p>{todo.text}</p>
+            <p className="category">({todo.category})</p>
+            
+          </div>
+          <button>Completar</button>
+          <button>x</button>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
